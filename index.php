9a92d2482f14b2aea62b92a88a8602af6f0e3bb0
cl-server <cl-server@ip-10-86-246-189.ec2.internal>
2013-06-25 07:47:35 +0000

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+     <title>Hello World</title>
+  </head>
+  <body>
+     <?php echo '<p>Hello World</p>'; ?>
+  </body>
+  <script>
+  alert("it is a new beggining !!");
+  console.log('PaaS:  appFog or openShift');
+  console.log('project->pass->update');
+  </script>
+</html>
